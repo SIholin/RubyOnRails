9c33038b4153e1b7a49adfc746929c92e693136d
SIholin <iholin.sanna@gmail.com>
2022-12-29 13:54:19 +0200

    viikko 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :destroy_session
+  helper_method :ensure_that_signed_in
 
   def current_user
     return nil if session[:user_id].nil?
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def destroy_session
     session[:user_id] = nil
+  end
+
+  def ensure_that_signed_in
+    redirect_to signin_path, notice: 'you should be singed in' if current_user.nil?
   end
 end
